7ddee8bad436086bb8566893a7f13dd7228e68d4
PajaspaceNet <elektrosoft.paja@gmail.com>
2025-01-08 19:30:06 +0100

    Update ovecka.py

--- ovecka.py
+++ ovecka.py
@@ -7,9 +7,9 @@ import os
 def circle_text(text, width=10, height=5, speed=0.2):
     for i in range(width + height):
         os.system('cls' if os.name == 'nt' else 'clear')
-        if i < width:  # Posun doprava
+        if i < width:  # GO rght
             print(" " * i + text)
-        elif i < width + height:  # Posun dolů
+        elif i < width + height:  # Go DOwn
             print("\n" * (i - width) + " " * width + text)
         time.sleep(speed)
 
@@ -19,9 +19,9 @@ def circle_text(text, width=10, height=5, speed=0.2):
 # ovecka.py
 from ovecka_splash import SplashScreen
 
-# Zobrazení splash screenu
-splash = SplashScreen(title="Ovečka Počítání", bg_color=(50, 50, 200))  # Modré pozadí
-splash.show(message="Ovečka Počítání... 🐑", font_size=50, duration=5)
+# Splash screen
+splash = SplashScreen(title="Counting Sheeps", bg_color=(50, 50, 200))  # Modré pozadí
+splash.show(message="Counting Sheeps... 🐑", font_size=50, duration=5)
 
 def nakreslena_ovecka():
     sheep_art = """
@@ -43,103 +43,101 @@ def nakreslena_ovecka2():
              ||     ||
        """
 
-    # Animace
+    # Animation
     for i in range(20):
         os.system('cls' if os.name == 'nt' else 'clear')
 
-        # Odsazení pro pohyb
+        # move
         print(" " * i + sheep_art2)
 
         time.sleep(0.2)
 
 
 
-# prehraje ovecku
+# sound
 def ovecka():
     # Inicializace mixeru
     pygame.mixer.init()
     # Načtení hudebního souboru
-    pygame.mixer.music.load("ovecka.mp3")  # Nahraď cestu svým souborem
-    # Přehrávání hudby
+    pygame.mixer.music.load("ovecka.mp3")  # You can modify whatever you want
+    # Music Play
     pygame.mixer.music.play()
-    # Udržení programu spuštěného, dokud se hudba nepřehraje
-   # while pygame.mixer.music.get_busy():
-    #    pass  # Čekej, dokud hudba hraje
+   
 
 def chrapani():
-        # Inicializace mixeru
+        # Mixer
         pygame.mixer.init()
-        # Načtení hudebního souboru
-        pygame.mixer.music.load("chrap.mp3")  # Nahraď cestu svým souborem
-        # Přehrávání hudby
+        # Sound Loading
+        pygame.mixer.music.load("chrap.mp3")  # You can modify whatever you want
+        # Music Play
         pygame.mixer.music.play()
-        # Udržení programu spuštěného, dokud se hudba nepřehraje
+        # Keep stay until play stop
         while pygame.mixer.music.get_busy():
-            pass  # Čekej, dokud hudba hraje
+            pass  # Wait
 
 
 def predchrap():
     # Inicializace mixeru
     pygame.mixer.init()
     # Načtení hudebního souboru
-    pygame.mixer.music.load("predchrap.mp3")  # Nahraď cestu svým souborem
-    # Přehrávání hudby
+    pygame.mixer.music.load("predchrap.mp3")  # You can choose what ever you want
+    # Music play
     pygame.mixer.music.play()
-    # Udržení programu spuštěného, dokud se hudba nepřehraje
+    # Keep stay until play stop
     while pygame.mixer.music.get_busy():
-        pass  # Čekej, dokud hudba hraje
+        pass  # Wait till play
 
 
 
 
 def nahodny_pes():
-    characters = ('pes zeryk', 'pes alik', 'kocour_v_botach', 'mickey_mouse', 'koucour_mikes', 'sponge_bob')
+    characters = ('dog zeryk', 'dog alik', 'cat_in_boots', 'mickey_mouse', 'cat_mikes', 'sponge_bob')
     random_char = random.choice(characters)
     return (f" {random_char}")
 
 def smilik():
-    sheep_emojis = ["!!!","normalne mi hrabe" ,"🐏","???" ,"🦙", "🐂" ,"jakto ze nechrapes" ,"🚀", "😄", "🤪", "🤓"]
+    sheep_emojis = ["!!!","Im really really crazy" ,"🐏","???" ,"🦙", "🐂" ,"sleep NOW" ,"🚀", "😄", "🤪", "🤓"]
     random_emoji = random.choice(sheep_emojis)
     return random_emoji
 
 def count_sheep(n):
-    print("Začínáme počítat ovečky...")
+    print("Start Counting Sheeps...")
     if n == 0:
         return ""
     result = ""
     for i in range(1, n + 1):
 
-        print(f"{i} ovecka..." , end=" ", flush=True)  # Vypiš okamžitě
-        time.sleep(2)  # Pauza na 1 sekundu
+        print(f"{i} sheeps..." , end=" ", flush=True)  # Write
+        time.sleep(2)  # Pause 1 sec
 
-        #result += (f"{i}  ovecka... ")
+       
         if (i % 3 == 0):
          predchrap()
 
-        #result += f"{nahodny_pes()} \n "
+        
          print(f"{nahodny_pes()} \n ")
          print(f'{smilik()}', end=" ")
          ovecka()
     return result
 
 nakreslena_ovecka()
-j= (input("jak moc se ti chce spat ? malo , dost , strasne ?" ))
+j= (input("How much you want to sleep? little , more , lot ?" ))
 
 
-if (j == "malo"):
+if (j == "little"):
     i = 10
-elif (j == "dost"):
+elif (j == "more"):
      i = 20
-elif (j == "strasne"):
+elif (j == "lot"):
       i = 30
 
 print(count_sheep(i))
-print (f"a ... nakonec {(nahodny_pes())}")
-print (f"takovych ovecek ..uz koukej chrapat ")
+print (f"and ... last {(nahodny_pes())}")
+print (f"sooo much sheeps  ..GO sleep GO GO GO  ")
 chrapani()
 print('🐑 🚀 😄 🤪 🤓')
 nakreslena_ovecka()
-circle_text("Dobrou noc", width=20, height=5, speed=0.1)
+circle_text("Good Night", width=20, height=5, speed=0.1)
 
 
 
